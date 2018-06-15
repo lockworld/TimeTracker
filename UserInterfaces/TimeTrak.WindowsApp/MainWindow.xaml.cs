70a268876fdb625f6bc61b1165847d15eddaf732
Doug Lockwood <dlockwood@allosource.org>
2018-06-15 09:08:43 -0600

    Some minor updates, especially to sourcelist.md

--- UserInterfaces/TimeTrak.WindowsApp/MainWindow.xaml.cs
+++ UserInterfaces/TimeTrak.WindowsApp/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,18 +26,40 @@ namespace TimeTrak.WindowsApp
         public string localDB;
         public MainWindow()
         {
-            localDB = Properties.Settings.Default.LocalDB;
-            InitializeComponent();
+            AddHotKeys();            
+        }
 
-            var key = new HotKey(
-                (ModifierKeys.Alt),
-                Key.A,
-                this,
-                delegate
-                {
-                    ShowMainWindow();
-                }
-            );
+        private void AddHotKeys()
+        {
+            try
+            {
+                // NOTE: Only works when window is in focus...looking into global hotkey management instead
+                RoutedCommand firstSettings = new RoutedCommand();
+                firstSettings.InputGestures.Add(new KeyGesture(Key.A, ModifierKeys.Alt));
+                CommandBindings.Add(new CommandBinding(firstSettings, HKShowWindow));
+
+                RoutedCommand secondSettings = new RoutedCommand();
+                secondSettings.InputGestures.Add(new KeyGesture(Key.B, ModifierKeys.Alt));
+                CommandBindings.Add(new CommandBinding(secondSettings, HKHideWindow));
+            }
+            catch (Exception err)
+            {
+                //handle exception error
+            }
+        }
+
+        private void HKShowWindow(object sender, ExecutedRoutedEventArgs e)
+        {
+            //handler code goes here.
+            System.Windows.Forms.MessageBox.Show("Alt+A key pressed");
+            ShowWindow();
+        }
+
+        private void HKHideWindow(object sender, RoutedEventArgs e)
+        {
+            //handler code goes here.
+            System.Windows.Forms.MessageBox.Show("Alt+B key pressed");
+            App.Current.MainWindow.Close();
         }
 
         private static void ShowMainWindow()
@@ -59,8 +82,36 @@ namespace TimeTrak.WindowsApp
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            e.Cancel = true;
+            MessageBox.Show("Hiding...");
             this.Hide();
+            e.Cancel = true;
+            Task.Delay(10000).ContinueWith(_ => ShowWindow());
+        }
+
+        private void ShowWindow()
+        {
+            MessageBox.Show("Showing...");
+            // create a thread  
+            Thread newWindowThread = new Thread(new ThreadStart(() =>
+            {
+                // create and show the window
+                MainWindow obj = new MainWindow();
+                obj.Show();
+                obj.Focus();
+
+                // start the Dispatcher processing  
+                System.Windows.Threading.Dispatcher.Run();
+            }));
+
+            // set the apartment state  
+            newWindowThread.SetApartmentState(ApartmentState.STA);
+
+            // make the thread a background thread  
+            newWindowThread.IsBackground = true;
+
+            // start the thread  
+            newWindowThread.Start();
+            
         }
     }
 }
--- sourcelist.md
+++ sourcelist.md
@@ -1,5 +1,5 @@
 #Sources Used in This Project#
-This document contains some sources used in researching concepts for this application. Some code examples are used as-is, others are modified beyond recognition to work with this particular application. The list is simply a reference to return to the point of origin at a later point in time.
+This document contains some sources used in researching concepts for this application. Some code examples are used as-is, others are modified beyond recognition to work with this particular application, and others are ignored completely. The list is simply a reference to return to the point of origin at a later point in time.
 
 **HotKey Registration**
 - https://bloggablea.wordpress.com/2007/05/01/global-hotkeys-with-net/
@@ -9,6 +9,7 @@ This document contains some sources used in researching concepts for this applic
 
 **Hide window on close**
 - https://stackoverflow.com/questions/638942/wpf-hide-on-close
+- https://stackoverflow.com/questions/2329978/the-calling-thread-must-be-sta-because-many-ui-components-require-this
 
 **Create a Windows Service**
 - https://docs.microsoft.com/en-us/dotnet/framework/windows-services/walkthrough-creating-a-windows-service-application-in-the-component-designer
@@ -24,4 +25,13 @@ This document contains some sources used in researching concepts for this applic
 - http://wixtoolset.org/
 - https://marketplace.visualstudio.com/items?itemName=RobMensching.WixToolsetVisualStudio2017Extension
 
+** Host a WCF Service inside a Managed Windows Service **
+*** This will be used to allow me to build a WPF/XAML application that the user can interact with, and allow it to communicate directly with the background TimeTrak service ***
+- https://docs.microsoft.com/en-us/dotnet/framework/wcf/feature-details/how-to-host-a-wcf-service-in-a-managed-windows-service
+- https://docs.microsoft.com/en-us/dotnet/framework/wcf/how-to-host-a-wcf-service-in-a-managed-application
+
+** Allow users to specify a URL for a web service or local service **
+- http://www.diogonunes.com/blog/calling-a-web-method-in-c-without-a-service-reference/
+- https://stackoverflow.com/a/5036342/5517386
+- https://docs.microsoft.com/en-us/dotnet/framework/wcf/specifying-an-endpoint-address
 
