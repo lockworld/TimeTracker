3f93981171382deb12a0408b6eccf4e8e9b02898
Doug Lockwood <dlockwood@allosource.org>
2018-10-31 11:06:20 -0600

    Created a master docked window and copied WpfAppBar from _SAMPLES to Core to be usable from all wpf applications.

---
+++ UserInterfaces/TimeTrak.DockedInterface/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WindowsAppBar;
+using System.Windows.Forms;
+using System.Windows.Media.Animation;
+
+namespace TimeTrak.DockedInterface
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DockPosition.ItemsSource = new[]
+            {
+                AppBarDockMode.Left,
+                AppBarDockMode.Right,
+                AppBarDockMode.Top,
+                AppBarDockMode.Bottom
+            };
+            DockMonitor.ItemsSource = MonitorInformation.GetAllMonitors();
+            DockMonitor.SelectedItem = MonitorInformation.GetAllMonitors().Where(m => m.IsPrimary).FirstOrDefault();
+
+            AppMenu.Background = SystemColors.ActiveBorderBrush;
+            AppMenuName.Content = this.Title;
+            
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void ThumbControl_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            double delta;
+            switch (DockMode)
+            {
+                case AppBarDockMode.Left:
+                    delta = e.HorizontalChange * -1;
+                    break;
+                case AppBarDockMode.Right:
+                    delta = e.HorizontalChange * -1;
+                    break;
+                case AppBarDockMode.Top:
+                    delta = e.VerticalChange * -1;
+                    break;
+                case AppBarDockMode.Bottom:
+                    delta = e.VerticalChange * -1;
+                    break;
+                default: throw new NotSupportedException();
+            }
+
+            
+            this.DockedWidthOrHeight += (int)(delta / VisualTreeHelper.GetDpi(this).PixelsPerDip);
+
+            // TODO...store last used width and set width or height to the appropriate saved value.
+        }
+
+        private void AppMenu_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Border sp = sender as Border;
+            DoubleAnimation db = new DoubleAnimation();
+            //db.From = 12;
+            db.To = 125;
+            db.Duration = TimeSpan.FromSeconds(0.5);
+            db.AutoReverse = false;
+            db.RepeatBehavior = new RepeatBehavior(1);
+            sp.BeginAnimation(StackPanel.HeightProperty, db);
+
+        }
+        private void AppMenu_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Border sp = sender as Border;
+            DoubleAnimation db = new DoubleAnimation();
+            //db.From = 12;
+            db.To = 25;
+            db.Duration = TimeSpan.FromSeconds(0.5);
+            db.AutoReverse = false;
+            db.RepeatBehavior = new RepeatBehavior(1);
+            sp.BeginAnimation(StackPanel.HeightProperty, db);
+
+        }
+    }
+}
---
+++ _Core/WindowsAppBar/MonitorInformation.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using static WindowsAppBar.AppBarMethods;
+
+namespace WindowsAppBar
+{
+    public sealed class MonitorInformation : IEquatable<MonitorInformation>
+    {
+        public Rect ViewportBounds { get; }
+
+        public Rect WorkAreaBounds { get; }
+
+        public bool IsPrimary { get; }
+
+        public string DeviceId { get; }
+
+        internal MonitorInformation(MONITORINFOEX mex)
+        {
+            this.ViewportBounds = (Rect)mex.rcMonitor;
+            this.WorkAreaBounds = (Rect)mex.rcWork;
+            this.IsPrimary = mex.dwFlags.HasFlag(MONITORINFOF.PRIMARY);
+            this.DeviceId = mex.szDevice;
+        }
+
+        public static IEnumerable<MonitorInformation> GetAllMonitors()
+        {
+            var monitors = new List<MonitorInformation>();
+            MonitorEnumDelegate callback = delegate (IntPtr hMonitor, IntPtr hdcMonitor, ref RECT lprcMonitor, IntPtr dwData)
+            {
+                MONITORINFOEX mi = new MONITORINFOEX();
+                mi.cbSize = Marshal.SizeOf(typeof(MONITORINFOEX));
+                if (!GetMonitorInfo(hMonitor, ref mi))
+                {
+                    throw new Win32Exception();
+                }
+                
+                monitors.Add(new MonitorInformation(mi));
+                return true;
+            };
+
+            EnumDisplayMonitors(IntPtr.Zero, IntPtr.Zero, callback, IntPtr.Zero);
+
+            return monitors;
+        }
+
+        public override string ToString() => DeviceId;
+
+        public override bool Equals(object obj) => Equals(obj as MonitorInformation);
+
+        public override int GetHashCode() => DeviceId.GetHashCode();
+
+        public bool Equals(MonitorInformation other) => this.DeviceId == other?.DeviceId;
+
+        public static bool operator ==(MonitorInformation a, MonitorInformation b)
+        {
+            if (ReferenceEquals(a, b))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(a, null))
+            {
+                return false;
+            }
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(MonitorInformation a, MonitorInformation b) => !(a == b);
+    }
+}
